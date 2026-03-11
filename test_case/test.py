1b475ddbc512f343d2a682ca164aa1b374a203bc
xiaotong.wang <18648483389@163.com>
2026-03-11 17:12:16 +0800

    chore: remove unused tet4_mat1.jsonc configuration file
    - Deleted the `tet4_mat1.jsonc` file as it is no longer needed for the current project setup, streamlining the repository and reducing clutter.

---
+++ test_case/test.py
@@ -0,0 +1,26 @@
+from cli_test_framework.runners.json_runner import JSONRunner
+from cli_test_framework.utils.report_generator import ReportGenerator
+import sys
+from pathlib import Path
+
+def main():
+    script_dir = Path(__file__).resolve().parent
+    runner = JSONRunner(config_file=str(script_dir / "test_cases.json"), workspace=str(script_dir))
+    success = runner.run_tests()
+    
+    # Generate and save the report
+    report_generator = ReportGenerator(runner.results, "test_case/test_report.txt")
+    report_generator.print_report()
+    report_generator.save_report()
+
+    runner1=JSONRunner(config_file=str(script_dir / "benchmark"/"test_cases.json"), workspace=str(script_dir))
+
+    success1 = runner1.run_tests()
+    report_generator1 = ReportGenerator(runner1.results, "test_case/test_benchamrk_report.txt")
+    report_generator1.print_report()
+    report_generator1.save_report()
+
+    sys.exit(0 if success and success1 else 1)
+
+if __name__ == "__main__":
+    main()
