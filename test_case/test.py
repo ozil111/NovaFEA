32e1f4458e07cf529b2627c4d5513cc7f26f60de
xiaotong.wang <18648483389@163.com>
2026-03-11 17:15:22 +0800

    chore: update test report file paths and remove obsolete benchmark report
    - Updated the paths for the test report files in `test.py` to ensure they are correctly referenced.
    - Deleted the obsolete `test_case/test_benchamrk_report.txt` file to reduce clutter in the repository.
    - Added new entries to `.gitignore` for the updated test report files to prevent them from being tracked.

--- test_case/test.py
+++ test_case/test.py
@@ -9,14 +9,14 @@ def main():
     success = runner.run_tests()
     
     # Generate and save the report
-    report_generator = ReportGenerator(runner.results, "test_case/test_report.txt")
+    report_generator = ReportGenerator(runner.results, str(script_dir / "test_report.txt"))
     report_generator.print_report()
     report_generator.save_report()
 
     runner1=JSONRunner(config_file=str(script_dir / "benchmark"/"test_cases.json"), workspace=str(script_dir))
 
     success1 = runner1.run_tests()
-    report_generator1 = ReportGenerator(runner1.results, "test_case/test_benchamrk_report.txt")
+    report_generator1 = ReportGenerator(runner1.results, str(script_dir / "test_benchmark_report.txt"))
     report_generator1.print_report()
     report_generator1.save_report()
 
